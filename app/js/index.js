ad00e7ccf64fa383699807af2a32204119b13da0
Aidyn-D <gurudeepsehra11@gmail.com>
2021-09-21 18:22:08 +0530

    mob menu
    touch-ups

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,15 @@
 import '../scss/style.scss';
 
+
+
+let x = document.getElementById("hamburger").addEventListener("click", mobmenu);
+let menu = document.getElementById("menu");
+
+function mobmenu() {
+    
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
